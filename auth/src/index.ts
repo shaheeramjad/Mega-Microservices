747b298280a62105fedf9629e989ebf7e9a3e963
shaheeramjad <shaheeransari450@gmail.com>
2026-07-11 12:48:13 +0500

    Response Normalization Done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import {signinRouter} from './routes/signin';
+import { errorHandler } from './middlewares/error-handler';
+import {NotFoundError} from './errors/not-found-error';
 const app = express();
 app.use(json());
 
@@ -11,6 +14,11 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 app.use(signinRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
 
 
 app.listen(3000, () => {
